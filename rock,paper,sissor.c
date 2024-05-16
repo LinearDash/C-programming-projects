978036d5f6468d1533854d2e18d1bf8415b061cf
Sandesh Adhikari <153103160+LinearDash@users.noreply.github.com>
2024-05-16 21:55:05 +0545

    Add files via upload

--- rock,paper,sissor.c
+++ rock,paper,sissor.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include <stdlib.h> 
 #include<time.h>
+#include<ctype.h>
 
 
 int main(){
     srand(time(NULL));
     int player_vlaue, computer_value;
-    printf("For rock(0) \nFor paper(1) \nFor sissor(2 \nPick One:)");
-    scanf("%d",&player_vlaue);
+    char respond=' ';
+    do
+    {
+       int player_vlaue, computer_value;
+       printf("For rock(0) \nFor paper(1) \nFor sissor(2) \nPick One:");
+       scanf("%d",&player_vlaue);
 
-    computer_value = rand()% 3;
+     computer_value = rand()% 3;
      if (computer_value==player_vlaue)
      {printf("It,s a tie\n");}
      else if (computer_value==0 && player_vlaue==1 ||
@@ -19,10 +24,14 @@ int main(){
        printf("You WON\n");}
      else
      printf("You LOST\n");
+     printf("\n Do you want to play again (Y/N): \n");
+     scanf("%c");
+     scanf("%c",&respond);
+     respond = toupper(respond);
      
+    } while (respond == 'Y');
+    printf("thanks for playing!");
      
-
-    
     return 0;
 
 }
