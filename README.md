50624b1cbc1c1fd600b2d8967ac5090c3d4e47d9
Sandesh <asandesh495@gmail.com>
2024-05-17 09:28:42 +0545

    new entry

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # C-programming-projects
+This is my project respo where i am attempting some small projects on c.
---
+++ tic-tac-toe.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>
+
+char board [3][3];
+const char PLAYER = 'X';
+const char COMPUTTER = 'O';
+
+void resetboard(){}
+void printboard(){}
+int checkboard(){}
+void playermove(){}
+void computermove(){}
+void checkwinner(){}
+void printwinner(char){}
+
+int main(){
+resetboard();
+printboard();
+
+return 0;
+}
+void resetboard(){}
+void printboard(){}
+int checkboard(){}
+void playermove(){}
+void computermove(){}
+void checkwinner(){}
+void printwinner(char){}
