38cbb0dfedaf6a96cf05793302a9ff7e8d9e042c
Sandesh <asandesh495@gmail.com>
2024-05-17 14:54:02 +0545

    rps

--- rock,paper,sissor.c
+++ rock,paper,sissor.c
@@ -11,10 +11,17 @@ int main(){
     do
     {
        int player_vlaue, computer_value;
-       printf("For rock(0) \nFor paper(1) \nFor sissor(2) \nPick One:");
+       printf("For rock(1) \nFor paper(2) \nFor sissor(3) \nPick One:");
        scanf("%d",&player_vlaue);
+       player_vlaue = player_vlaue-1 ;
 
      computer_value = rand()% 3;
+      if(computer_value==0){
+         printf("Computer picked Rock\n");
+         }else if(computer_value==1){
+          printf("Computer picked paper\n");
+         }else printf("Computer picked sissor\n");
+          
      if (computer_value==player_vlaue)
      {printf("It,s a tie\n");}
      else if (computer_value==0 && player_vlaue==1 ||
